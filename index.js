3fb3412f6b94b44e7683c25a4f23942cccf0ed1a
Diego Valero <diego27jay@gmail.com>
2022-01-10 17:13:30 -0700

    commit tree cba15415d68f6134d770c838163dfffb69f0fbd0

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  if (rideLength <= 400) {
+    return "This one is on me!";
+  } else if (rideLength > 2000 && rideLength <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (rideLength > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city == "NYC" ? "Ok, sounds good." :"No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  let message;
+  switch (tipAmount) {
+    case "generous":
+      message = "Thank You so much.";
+      break;
+  }
 }
